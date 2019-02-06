4b89bfcfd8b598a83c921ce7389e7013099559e6
kvprasad505 <kvprasad505@gmail.com>
2019-02-06 20:15:53 +0530

    second commit

---
+++ app/src/main/java/com/example/user1/musicplayer/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.user1.musicplayer;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    ListView Lv;
+    String[] items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+        Lv = (ListView) findViewById(R.id.Lv);
+        final ArrayList<File> mysongs = findSongs(Environment.getExternalStorageDirectory());
+        items = new String[ mysongs.size()];
+
+
+        for (int i = 0; i < mysongs.size(); i++) {
+            items[i] = mysongs.get(i).getName().toString().replace(".mp3", "").replace("wav", "");
+
+
+        }
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(getApplication(),R.layout.song_layout,R.id.txt,items);
+
+        Lv.setAdapter(adp);
+
+        Lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),player.class).putExtra("pos",position).putExtra("songlist",mysongs);
+
+                intent.putExtra("song_name",items[position]);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public ArrayList<File> findSongs(File root) {
+        ArrayList<File> al = new ArrayList<File>();
+        File[] files = root.listFiles();
+        for (File singleFile : files) {
+            if (singleFile.isDirectory() && !singleFile.isHidden()) {
+                al.addAll(findSongs(singleFile));
+
+            } else {
+                if (singleFile.getName().endsWith(".mp3")|| singleFile.getName().endsWith(".wav")) {
+                    al.add(singleFile);
+                }
+            }
+
+
+        }
+        return al;
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+          if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+              Intent share=new Intent(Intent.ACTION_SEND);
+              share.setType("Text/plain");
+              String sharebody = "your body here";
+              String sharesub = "your subject here";
+              share.putExtra(Intent.EXTRA_SUBJECT,sharebody);
+              share.putExtra(Intent.EXTRA_TEXT,sharesub);
+              startActivity(Intent.createChooser(share,"Share Using"));
+
+
+        } else if (id == R.id.nav_send) {
+            Intent intent=new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.google.com/gmail/"));
+            startActivity(intent);
+
+        }
+          else if (id == R.id.nav_help) {
+              Intent intent=new Intent(Intent.ACTION_VIEW);
+              intent.setData(Uri.parse("https://www.musicplayer.io/privacy.html"));
+              startActivity(intent);
+
+          }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
